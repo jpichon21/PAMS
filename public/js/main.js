151756719244e3269c018dc0ac4350ef41a6da7f
jpichon21 <juliencfpichon@gmail.com>
2019-03-26 15:21:06 +0100

    Inté V1 page Création pams (toggle disposition OK)

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,16 @@ $(document).ready(function() {
         document.getElementById('bodyTimer').classList.add('hidetimer');
        }, 5000);
 });
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+function toggleFunction() {
+    var x = document.getElementById("dispositionContainer");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
