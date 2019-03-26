55bbc0b845d393eec948660a6b5bf3effd49e905
jpichon21 <juliencfpichon@gmail.com>
2019-03-26 18:02:49 +0100

    Ajout dropdown Choix de l'arrière plan V1

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,8 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
-function toggleFunction() {
+/*Test switch toggle Nav creation Pam's */
+function dispositionToggle() {
     var x = document.getElementById("dispositionContainer");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -16,3 +17,31 @@ function toggleFunction() {
       x.style.display = "none";
     }
   }
+
+function backgroundToggle() {
+    var x = document.getElementById("backgroundContainer");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+var slider = document.getElementById("opacityRange");
+var output = document.getElementById("opacityValue");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+/*
+var _actionToDropZone = $("#form_snippet_image").attr('action');
+
+Dropzone.autoDiscover = false;
+var myDropzone = new Dropzone("#form_snippet_image", { url: _actionToDropZone });
+
+myDropzone.on("addedfile", function(file) {
+  alert('nouveau fichier reçu');
+});*/
