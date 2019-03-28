723d63734ca42cddfa86e7ba26072da8d8c16cae
jpichon21 <juliencfpichon@gmail.com>
2019-03-28 13:26:59 +0100

    Intégration Layouts + Sélections Temps réel

---
+++ public/js/createpams.js
@@ -0,0 +1,79 @@
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(document).on('click', '#backgroundToggle', function () {
+    backgroundPopupToggle();
+    return false;
+});
+
+$(document).on('click', '#dispositionToggle', function () {
+    dispositionPopupToggle();
+    return false;
+});
+
+/**Toggle tempsréel des layouts***/
+$(document).on('change', '.input-disposition', function(e) {
+	var $this = $(this);
+	var value = $this.val();
+	toggleDisposition(value);
+});
+
+
+
+/*opacity*/
+var slider = document.getElementById("opacityRange");
+var output = document.getElementById("opacityValue");
+output.innerHTML = slider.value; 
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+   /**************
+     * Functions*
+    ****************/
+
+    /**Gestion des popups*/
+
+function backgroundPopupToggle() {
+    var x = document.getElementById("backgroundContainer");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+function dispositionPopupToggle() {
+    var x = document.getElementById("dispositionContainer");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  /*Layouts*/
+  function toggleDisposition(disposition) {
+	var $active = $('.disposition-active');
+	var $target = $('#'+disposition);
+
+	// Si la sélection est celle déjà active, on fait rien
+	if ($target.is($active)) return false;
+
+	// Si déjà une d'active
+	if ($active.length > 0) {
+		$active.removeClass('disposition-active');
+	}
+	$target.addClass('disposition-active');
+}
+
+
+
+
+
+
