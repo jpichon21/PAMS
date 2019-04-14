1889af6784284f4b74a2abbe5cd9c78f479d1918
jpichon21 <juliencfpichon@gmail.com>
2019-04-14 11:10:10 +0200

    save bg image from id

--- public/js/view-pams.js
+++ public/js/view-pams.js
@@ -2,16 +2,27 @@
 $( document ).ready(function() {
 
     var pamsJson = JSON.parse(pamsJs)
+    console.log(pamsJson);
     var disposition = pamsJson.layout;
     var backgroundColor = pamsJson.backgroundColor;
     var backgroundOpacity = pamsJson.backgroundOpacity
+    var backgroundImage = pamsJson.backgroundImage;
+    var uploadedbackgroundImage = pamsJson.uploadedbackgroundImage;
+    var blockText = {};
+    
     
     /*check*/
     console.log(disposition);
     console.log(backgroundColor);
+    console.log(backgroundImage);
 
     defineDisposition(disposition);
     defineBackgroundColor(backgroundColor, backgroundOpacity);
+
+
+    if ( backgroundImage !== null){
+        defineBackgroundImage(backgroundImage);
+    }
 
 });
 
@@ -29,4 +40,10 @@ function defineDisposition(disposition) {
 function defineBackgroundColor(backgroundColor, backgroundOpacity) {
     document.getElementById("createBody").style.backgroundColor = backgroundColor;
     document.getElementById("createBody").style.opacity = backgroundOpacity;
+}
+
+function defineBackgroundImage(backgroundImage, backgroundOpacity){
+    var file = $('#' + backgroundImage + 'input').val();
+    document.getElementById('createBody').style.backgroundImage = "url(" + file + ")";
+    console.log(file);
 }
